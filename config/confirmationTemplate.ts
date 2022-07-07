540e8442c19a9a2eeed3de61f62a304b0c212c02
Filip <filip@filip.home>
2022-07-07 13:20:48 +0200

    fix confirmation email template

--- config/confirmationTemplate.ts
+++ config/confirmationTemplate.ts
@@ -1,5 +1,4 @@
 export const confirmation = `
 <p>Dziękujemy za kontakt z Obczaj Matme! W najbliższym czasie ktoś się do ciebie odezwie żeby odpowiedzieć na twoje pytania!</p>
-<p style='margin-top: 1rem;'>Pozdrawiamy</p>
-<p>Zespół Obczaj Matme</p>
+<p style='margin-top: 16px;'>Pozdrawiamy <br>Zespół Obczaj Matme</p>
 `
